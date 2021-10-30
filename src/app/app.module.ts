50604c38782a342d81dffa3554270b7ef7608d48
atillaost <atilla.gueven@ost.ch>
2021-10-30 11:52:39 +0200

    Story 3.1 Fixes #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
 import { FormsModule } from '@angular/forms';
 import { ChatBarComponent } from './chat-bar/chat-bar.component';
 import { ChatHistoryComponent } from './chat-history/chat-history.component';
@@ -14,7 +13,6 @@ import { ChatHistoryComponent } from './chat-history/chat-history.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent,
     ChatBarComponent,
     ChatHistoryComponent
   ],
